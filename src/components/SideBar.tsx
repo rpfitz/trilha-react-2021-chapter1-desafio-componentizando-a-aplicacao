259a9e7d41f1122b2e37ebd3fca74cb375ff8db6
Rafhael Fitzpatrick <rafhaelfitzpatrick@gmail.com>
2023-03-05 02:24:35 -0300

    Submit

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,28 @@
+import { useContext } from 'react';
+import { MoviesContext } from '../contexts/MoviesContext';
+import { Button } from './Button';
+
 export function SideBar() {
-  // Complete aqui
+  const { genres, handleClickButton, selectedGenreId } = useContext(
+    MoviesContext,
+  );
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
+
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
